ac8540074c44bf48885d7ae6359f0083b80439bc
rajvardhan zade <rajvardhanzade1819@gmail.com>
2026-04-19 23:44:37 +0530

    made a route to see users posts

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from "express";
 import multer from "multer";
 import imagekit from "./service.js";
 import connetDB from "./config/db.js";
-import {register,ImageData} from "./model/model.js"
+import {register,imageData} from "./model/model.js"
 import jwt from "jsonwebtoken"
 import cookieParser from "cookie-parser";
 
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieParser());
 connetDB();
 
-// add imagedata model and insert image url and user id in db
+
 
 app.post("/send", upload.single('img'), authMID ,async (req, res) => {
     try {
@@ -26,11 +26,16 @@ app.post("/send", upload.single('img'), authMID ,async (req, res) => {
             file: req.file.buffer,
             fileName: req.file.originalname
         });
+            let uurl = result.url
+            let uuid = req.user.id
+            let uuname = req.user.name
+            //console.log(uurl,uuid,uuname)
+        let data = await imageData({url:uurl,user:uuid,username:uuname})
+        await data.save()
 
         res.json({
             message: "Upload successful",
-            url: result.url,
-            id : req.user.id
+            data : data
         });
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -61,14 +66,14 @@ app.post("/login", async (req, res) => {
         if (result.password != password) {
             return res.json({ message: "password is incorrect!" })
         }
-        let token = jwt.sign({ id: result._id }, process.env.JWT_SECRET, { expiresIn: "1h" })
+        let token = jwt.sign({ id: result._id , name:result.username}, process.env.JWT_SECRET, { expiresIn: "1h" })
         res.cookie("token", token, {
             httpOnly: true,
             secure: false,
             maxAge: 60 * 60 * 1000
         });
         return res.status(200).json({
-            message: "Login successful", token
+            message: "Login successful"
         });
 
 
@@ -78,10 +83,18 @@ app.post("/login", async (req, res) => {
 
 })
 
+app.get("/images",authMID,async (req,res)=>{
 
+    try{
+            let data = await imageData.find({user: req.user.id})
+            if (data == []){
+                return res.json({message:"uploaded images here"})
+                           }
+            res.json({data:data})
+    }catch(error){res.status(404).json({message:"error occured!"})}
+})
 
-
-
+// for public route to fetch all images use .find({}) method try to use pagination
 app.listen(3000, () => {
     console.log("app is running")
 }) 
--- model/model.js
+++ model/model.js
@@ -4,13 +4,13 @@ const authSchema = mongoose.Schema({
     username : {type:String,unique:true},
     password : {type:String}
 })
+export const register = mongoose.model("register",authSchema)
 
 const imageSchema = mongoose.Schema({
     url : {type:String},
-    user: {type: mongoose.Schema.Types.ObjectId,ref:register}
+    user: {type: mongoose.Schema.Types.ObjectId,ref:register},
+    username : {type:String}
 })
+export const imageData = mongoose.model("imageData",imageSchema)
 
-const register = mongoose.model("register",authSchema)
-const imageData = mongoose.model("imageData",imageSchema)
 
-export default {register , imageData};
